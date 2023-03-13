6ccced660ec3a7bede45d5bb127a04fedab39e5e
Ilham <114786336+IlhamGumelar14@users.noreply.github.com>
2023-03-13 09:47:39 +0700

    Update About.js

--- src/views/About.js
+++ src/views/About.js
@@ -14,7 +14,7 @@ export default function About() {
                   <img
                     src={User}
                     alt="Profile"
-                    className="w-[60%] h-[60%] md:flex-auto scale-100 object-top pt-2 object-cover mx-auto grayscale"
+                    className="w-full h-full scale-100 object-bottom pt-5 object-cover mx-auto grayscale"
                   />
                 </div>
               </Fade>
